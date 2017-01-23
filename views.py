86b87e6951db6252bd2cbd84d4255a792a414212
yoshiki <yoshiki@umich.edu>
2017-01-23 12:50:31 -0500

    idk

--- views.py
+++ views.py
@@ -1,13 +1,10 @@
 from flask import render_template, request
 from app import app
-from schedule_api import *
 
 
 @app.route('/')
 def index():
     data = {}
-
-    data['terms'] = get_terms()
 
     return render_template('index.html', **data)
 
