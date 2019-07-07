7579b0caab8d9c2a1d4701f7a58273ca931a5b67
yoshiki <yoshiki@umich.edu>
2019-07-06 22:09:21 -0400

    Updates

---
+++ precache-manifest.acfdc17364a93c371dcfbbc08e3ffb71.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3bf49bf7266637567cbedaf30a74856",
+    "url": "/index.html"
+  },
+  {
+    "revision": "211b073f1c89b7fa4ff6",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "dca4eeb1e30b88d0ec21",
+    "url": "/static/css/main.c08c60aa.chunk.css"
+  },
+  {
+    "revision": "211b073f1c89b7fa4ff6",
+    "url": "/static/js/2.827d37b5.chunk.js"
+  },
+  {
+    "revision": "dca4eeb1e30b88d0ec21",
+    "url": "/static/js/main.e0206f52.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "de6b3cfc8d157b211af13e1c6e34d2e5",
+    "url": "/static/media/10_lick.de6b3cfc.JPG"
+  },
+  {
+    "revision": "e54763421d258f2a88b5591305990933",
+    "url": "/static/media/11_broken.e5476342.JPG"
+  },
+  {
+    "revision": "2112259caf8bd3c9192fffc1bcbcf805",
+    "url": "/static/media/12_nude.2112259c.JPG"
+  },
+  {
+    "revision": "41d9f12d066d41f3d48d0ea6d5985f4e",
+    "url": "/static/media/13_me.41d9f12d.JPG"
+  },
+  {
+    "revision": "3d323eb6db464c84dafd1a9e2f8ec1eb",
+    "url": "/static/media/14_grandpa.3d323eb6.JPG"
+  },
+  {
+    "revision": "a32d54af5ade153b2cf4173f79d8823c",
+    "url": "/static/media/15_lounging.a32d54af.jpg"
+  },
+  {
+    "revision": "20eab3730e199c8302e69af9377a24f3",
+    "url": "/static/media/16_lines.20eab373.jpg"
+  },
+  {
+    "revision": "850ed017dd3171beebb96e1e827d2558",
+    "url": "/static/media/1_Drenched.850ed017.JPG"
+  },
+  {
+    "revision": "7eb93bbbbfa62cc5fecb1f2a47b382d7",
+    "url": "/static/media/2_dane.7eb93bbb.jpg"
+  },
+  {
+    "revision": "8708e72e4a4fd307827fd361489d4f7a",
+    "url": "/static/media/3_rg.8708e72e.jpg"
+  },
+  {
+    "revision": "6ff7701c64be79e9bf05f1708367fec2",
+    "url": "/static/media/4_hair.6ff7701c.jpg"
+  },
+  {
+    "revision": "9050be28a0fb475ba5b9d1e3015b72d1",
+    "url": "/static/media/5_Dazed.9050be28.JPG"
+  },
+  {
+    "revision": "67c3616fc3689e2b52b75244442742cc",
+    "url": "/static/media/6_backside.67c3616f.JPG"
+  },
+  {
+    "revision": "3a4e7e1f0d9f599c93f5186da299ea78",
+    "url": "/static/media/7_backside2.3a4e7e1f.jpg"
+  },
+  {
+    "revision": "fc6696cac222e2666d4e70f5d5d6604b",
+    "url": "/static/media/8_feet.fc6696ca.JPG"
+  },
+  {
+    "revision": "e3108fa1731b712c2ad895114bf83163",
+    "url": "/static/media/9_hands.e3108fa1.JPG"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
